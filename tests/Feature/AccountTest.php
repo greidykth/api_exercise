b9e551aa3976cc74adf7a86cc6cc8237890fdba1
Greidy Peña <greidykp@gmail.com>
2022-11-15 16:24:21 -0500

    Add withdraw event endpoint for non-exisiting and existing account

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -38,6 +38,7 @@ class AccountTest extends TestCase
         $response = $this->post('/event', ["type"=>"deposit", "destination"=>"100", "amount"=>10]);
         $response->assertStatus(201)->assertJson(["destination"=> ["id"=>"100", "balance"=>10]]);
     }
+
     /**
      * Test for deposit into existing account
      *
@@ -48,6 +49,7 @@ class AccountTest extends TestCase
         $response = $this->post('/event', ["type"=>"deposit", "destination"=>"100", "amount"=>10]);
         $response->assertStatus(201)->assertJson(["destination"=> ["id"=>"100", "balance"=>20]]);
     }
+
     /**
      * Test for get balance for existing account.
      *
@@ -57,5 +59,27 @@ class AccountTest extends TestCase
     {
         $response = $this->get('/balance?account_id=100');
         $response->assertStatus(200)->assertContent('20');
+    }
+
+     /**
+     * Test for withdraw from non-existing account
+     *
+     * @return void
+     */
+    public function test_withdraw_from_non_existing_account()
+    {
+        $response = $this->post('/event', ["type"=>"withdraw", "origin"=>"200", "amount"=>"10"]);
+        $response->assertStatus(404)->assertContent('0');
+    }
+
+    /**
+     * Test for withdraw from existing account
+     *
+     * @return void
+     */
+    public function test_withdraw_from_existing_account()
+    {
+        $response = $this->post('/event', ["type"=>"withdraw", "origin"=>"100", "amount"=>5]);
+        $response->assertStatus(201)->assertJson(["origin"=> ["id"=>"100", "balance"=>15]]);
     }
 }
